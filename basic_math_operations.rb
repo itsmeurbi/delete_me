5b8d4b021f1b597ec32576cad28b685b5bcc2eb1
Miguel Angel Urbina Equihua <miguel.urbina15.1997@gmail.com>
2021-06-24 22:00:57 -0500

    Refator basic operations function

--- basic_math_operations.rb
+++ basic_math_operations.rb
@@ -1,11 +1,11 @@
 def basic_op(operator, number1, number2)
   if operator == '+'
-    return number1 + number2
+    number1 + number2
   elsif operator == '-'
-    return number1 - number2
+    number1 - number2
   elsif operator == '*'
-    return number1 * number2
+    number1 * number2
   else
-    return number1 / number2
+    number1 / number2
   end
 end
